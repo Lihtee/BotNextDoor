93bcdcb7966edd57b0055ade60d4d39d986a096c
DreySpi <andriocactus@gmail.com>
2021-09-28 22:43:17 +0500

    Global exception handling

--- BotNextDoor/CommandHandler.cs
+++ BotNextDoor/CommandHandler.cs
@@ -55,10 +55,18 @@ namespace BotNextDoor
 
             // Execute the command with the command context we just
             // created, along with the service provider for precondition checks.
-            await _commands.ExecuteAsync(
-                context, 
-                argPos,
-                null);
+            try
+            {
+                await _commands.ExecuteAsync(
+                    context, 
+                    argPos,
+                    null);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"spi{Environment.NewLine}{e}");
+            }
+            
         }
     }
 }
